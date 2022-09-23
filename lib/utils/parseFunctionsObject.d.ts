e000c53ec8929287ac034ce12b82f70432c8b34e
alandev2 <alanworking1@gmail.com>
2022-09-23 11:07:54 -0300

    feat: add v1 with strucute and esbuild

--- lib/utils/parseFunctionsObject.d.ts
+++
@@ -1,6 +0,0 @@
-export = parseFunctionsObject;
-declare function parseFunctionsObject(functionsObject: any): {
-    name: string;
-    filename: string;
-    folder: string;
-}[];
--- src/index.js
+++ src/index.js
@@ -1,55 +1,87 @@
-// const log = require('log-update');
-const path = require('path');
-const YAML = require('yaml');
-const parseFunctionsObject = require('./utils/parseFunctionsObject');
-const buildFiles = require('./utils/buildFiles');
-const convertFilesToZip = require('./utils/convertFilesToZip');
-const chalk = require('chalk');
-const fs = require('fs');
+// @ts-check
+const { readdirSync } = require('fs')
 
-const bytes = require('bytes');
+const buildESBuildLambda = require('./builders/esbuild')
+const getFiles = require('./utils/get-files')
+const parseHandlersFiles = require('./utils/parse-handlers-files')
+const parseHandlersYML = require('./utils/parse-handlers-yml')
+const zipDirectory = require('./utils/zip-directory')
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
-// program
-//   .option('-o, --output <path>', 'output path', 'build')
-//   .option('-f, --file <path>', 'file path')
-//   .option('-i, --individually', 'build individually lambdas')
-//   .option('-z, --zip', 'zip build')
-//   .option('-s, --source-map', 'enable source map file')
-//   .option('-m, --minify', 'enable minify file')
-//   .option('-t, --target <es>', 'ES version target', 'es2019');
+const builders = {
+  esbuild: buildESBuildLambda,
+}
 
-const options = {
-  file: 'functions.yml',
-  output: 'build',
-  minify: true,
-  sourceMap: true,
-  individually: true,
-  zip: true,
-  target: 'es2019'
-};
-
-
-const fileYml = fs.readFileSync(
-  path.resolve(process.cwd(), options.file),
-  'utf8',
-);
-
-const parsedYaml = YAML.parse(fileYml);
-const files = parseFunctionsObject(parsedYaml.functions);
-
-
-buildFiles(files, options).then(async () => {
-  if (options.zip && !options.individually) {
-    await convertFilesToZip(options.output, 'latest')
-    console.log('ok')
-    // log(
-    //   'latest.zip',
-    //   chalk.black.bgBlueBright(
-    //     'Size',
-    //     bytes(fs.statSync(outputZip).size),
-    //   ),
-    // );
-    // log.done();
+/**
+ * @typedef {{
+ *  functions?: string,
+ *  builder?: string,
+ *  outdir?: string,
+ *  base_dir?: string,
+ *  sourcemap?: boolean,
+ *  bundle?: boolean,
+ *  individually?: boolean,
+ *  zip?: boolean,
+ *  three_shaking?: boolean,
+ *  minify?: boolean
+ * }} BuildOptions
+ *
+ * @param {BuildOptions} options
+ */
+const buildRun = async (options = {}) => {
+  const buildOptions = {
+    functions: options.functions ?? 'serverless.yml',
+    builder: options.builder ?? 'esbuild',
+    outdir: options.outdir ?? 'build',
+    base_dir: options.base_dir ?? 'src',
+    sourcemap: options.sourcemap ?? true,
+    bundle: options.bundle ?? true,
+    individually: options.individually ?? true,
+    zip: options.zip ?? true,
+    three_shaking: options.three_shaking ?? false,
+    minify: options.minify ?? false,
+    target: 'es2019'
   }
-});
+
+  const builder = builders[buildOptions.builder ?? 'esbuild']
+
+  if (buildOptions.individually) {
+    const handlers = parseHandlersYML(buildOptions.functions)
+
+    await builder({
+      handlers,
+      build: buildOptions,
+    })
+  } else {
+    const files = await getFiles(buildOptions.base_dir)
+    const handlers = parseHandlersFiles(files)
+
+    await builder({
+      handlers,
+      build: buildOptions,
+    })
+  }
+
+  if (buildOptions.zip) {
+    const handlers = readdirSync(buildOptions.outdir)
+    console.debug(`Starting lambdas compress: - ${handlers.length} function(s)`)
+
+    const promises = []
+    for (const handler of handlers) {
+      const inputFolder = `${process.cwd()}/${buildOptions.outdir}/${handler}`
+      const outZip = `${buildOptions.outdir}/${handler}.zip`
+
+      const promiseResult = zipDirectory(inputFolder, outZip)
+      promises.push(promiseResult)
+
+      await sleep(200)
+    }
+
+    await Promise.all(promises)
+  }
+
+  console.info('Build finished')
+}
+
+buildRun({})
